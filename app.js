013383e98c4e6cc9546081806c46e4ccc4b8f306
Arjun M <arjunmanikkoth28@gmail,com>
2024-01-29 23:52:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//database connection
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/everbo_Ecommerce");
+
+const express = require("express");
+const app = express();
+
+//importing userrouter
+const userRoute=require("./routes/userRoute")
+
+//for all user routes
+app.use("/", userRoute);
+
+
+
+//creating server
+const PORT = 3000;
+app.listen(PORT, () => { 
+
+  console.log("server running at the port http://localhost:3000");
+
+})
