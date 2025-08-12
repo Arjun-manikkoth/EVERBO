eb8967ebd721d686dbf864d5a13e0c9c22d65909
Arjun-Manikkoth <arjunmanikkoth28@gmail.com>
2025-08-12 20:34:43 +0530

    fix : fix common js export error

--- constants/messages.js
+++ constants/messages.js
@@ -1,9 +1,9 @@
-export const ADDRESS_MESSAGES = {
+const ADDRESS_MESSAGES = {
     NOT_ADDED: "Address not added",
     ADDRESS_EXISTS: "Address already exists",
 };
 
-export const AUTH_MESSAGES = {
+const AUTH_MESSAGES = {
     WRONG_CREDENTIALS: "Invalid credentials",
     ACCOUNT_NOT_FOUND: "Account doesnot exist",
     ACCOUNT_EXISTS: "Already a user",
@@ -11,55 +11,75 @@ export const AUTH_MESSAGES = {
     REGISTRATION_FAILED: "Registration failed",
 };
 
-export const OTP_MESSAGES = {
+const OTP_MESSAGES = {
     OTP_EXPIRED: "OTP expired",
     INVALID_OTP: "Invalid OTP",
 };
 
-export const PASSWORD_MESSAGES = {
+const WISHLIST_MESSAGES = {
+    ADD_FAILED: "Couldn't add item",
+};
+
+const PASSWORD_MESSAGES = {
     NEW_PASSWORD_LOGIN: "Please login with your new password",
     PASSWORD_RESET_FAILED: "Password reset failed",
     CONFIRM_PASSWORD_FAILED: "Please Check the password",
 };
 
-export const EMAIL_MESSAGES = {
+const EMAIL_MESSAGES = {
     CHECK_MAIL: "Please check your Email for the link",
     MAIL_ACCOUNT_CHECK_FAILED: "Couldn't find an account with specified Email address",
 };
 
-export const BANNER_MESSAGES = {
+const BANNER_MESSAGES = {
     BANNER_ADDED: "Banner added successfully",
 };
 
-export const CART_MESSAGES = {
+const CART_MESSAGES = {
     FAILED_ADD_TO_CART: "Couldn't add item",
     FAILED_REMOVE_FROM_CART: "Couldnt remove product",
 };
 
-export const CATEGORY_MESSAGES = {
+const CATEGORY_MESSAGES = {
     NOT_ADDED: "Address not added",
     CATEGORY_EXISTS: "Category already exists",
     CATEGORY_ADD_FAILED: "Failed to add category",
     CATEGORY_UPDATE_FAILED: "Couldn't update category",
 };
 
-export const COUPON_MESSAGES = {
+const COUPON_MESSAGES = {
     NO_COUPONS: "No Coupons are added yet",
     COUPON_EXISTS: "Coupon already added",
     COUPON_ADD_FAILED: "Failed to add coupon",
 };
 
-export const ORDER_MESSAGES = {
+const ORDER_MESSAGES = {
     NO_ORDERS: "No Recent Orders",
 };
 
-export const COMMON_MESSAGES = {
+const COMMON_MESSAGES = {
     INTERNAL_SERVER_ERROR: "Internal Server Error",
 };
 
-export const PRODUCT_MESSAGES = {
+const PRODUCT_MESSAGES = {
     NO_PRODUCTS: "No Products Added",
     PRODUCT_EXISTS: "Product already added",
     PRODUCT_UPDATE_FAILED: "Couldn't update product",
     PRODUCT_ADD_FAILED: "Failed to add product",
+};
+
+module.exports = {
+    ADDRESS_MESSAGES,
+    AUTH_MESSAGES,
+    OTP_MESSAGES,
+    WISHLIST_MESSAGES,
+    PASSWORD_MESSAGES,
+    EMAIL_MESSAGES,
+    BANNER_MESSAGES,
+    CART_MESSAGES,
+    CATEGORY_MESSAGES,
+    COUPON_MESSAGES,
+    ORDER_MESSAGES,
+    COMMON_MESSAGES,
+    PRODUCT_MESSAGES,
 };
